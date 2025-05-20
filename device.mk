19c36905d553fcb3efc437d5e69f0e51f5df4681
Rocky7842 <eric.rocky7842@gmail.com>
2025-05-20 15:19:24 +0800

    psyche: Build Dolby audio
    Change-Id: I9395fb0ac37d9a3930145cce062732cf7396c87f

--- device.mk
+++ device.mk
@@ -21,6 +21,9 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
 
+# Audio configs (Dolby)
+TARGET_DOLBY_DAX_CONFIG := $(LOCAL_PATH)/audio/dolby/dax-default.xml
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
